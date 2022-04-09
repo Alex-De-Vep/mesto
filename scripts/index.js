45b6ea7b71ac1e836ec7f6dc3f31228829c18a4f
Alex Pavlov <alex.pavlov.948@gmail.com>
2022-04-09 19:51:03 +0300

    - изменил струтуру модуля - добавил функционал

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,29 @@
-//Общие для всех попапов
+// Объявление переменных
+const profilePopup = document.querySelector("#profile-popup");
+const profileName = document.querySelector(".profile__title");
+const profileText = document.querySelector(".profile__text");
+const profileForm = profilePopup.querySelector(".popup__form");
+const nameInput = profileForm.querySelector("[data-name]");
+const jobInput = profileForm.querySelector("[data-job]");
+const openProfilePopupButton = document.querySelector("[data-popup-profile]");
+
+const listCards = document.querySelector(".trips__list");
+const tripPopup = document.querySelector("#trip-popup");
+const tripForm = tripPopup.querySelector(".popup__form");
+const titleInput = tripForm.querySelector("[data-title]");
+const linkInput = tripForm.querySelector("[data-link]");
+const submitFormButton = tripForm.querySelector(".popup__button");
+const openTripPopupButton = document.querySelector("[data-popup-trip]");
+
+const imagePopup = document.querySelector("#image-popup");
+const popupImage = imagePopup.querySelector(".popup__image");
+
+const closeButtons = document.querySelectorAll("[data-close-popup]")
+
+const cardTemplate = document.querySelector('#card-template').content;
+
+
+//Функции
 
 const openPopup = (popup) => {
     popup.classList.add("popup_opened");
@@ -22,26 +47,9 @@ const closeByEscape = (event) => {
     }
 }
 
-const closeButtons = document.querySelectorAll("[data-close-popup]")
 closeButtons.forEach((button) => {
     const popup = button.closest(".popup");
     button.addEventListener("click", () => closePopup(popup));
-})
-
-//Попап редактирования профиля
-
-const profilePopup = document.querySelector("#profile-popup");
-const nameInput = profilePopup.querySelector("[data-name]");
-const jobInput = profilePopup.querySelector("[data-job]");
-const profileName = document.querySelector(".profile__title");
-const profileText = document.querySelector(".profile__text");
-
-const openProfilePopupButton = document.querySelector("[data-popup-profile]");
-openProfilePopupButton.addEventListener("click", () => {
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileText.textContent;
-    openPopup(profilePopup);
-    document.addEventListener("keyup", closeByEscape);
 })
 
 function handleProfileFormSubmit(evt) {
@@ -52,26 +60,6 @@ function handleProfileFormSubmit(evt) {
 
     closePopup(profilePopup);
 }
-const profileForm = profilePopup.querySelector(".popup__form");
-profileForm.addEventListener('submit', handleProfileFormSubmit);
-profilePopup.addEventListener("click", closeByOverlay);
-
-//Попап добавления карточки
-
-const listCards = document.querySelector(".trips__list");
-const tripPopup = document.querySelector("#trip-popup");
-const tripForm = tripPopup.querySelector(".popup__form");
-const titleInput = tripForm.querySelector("[data-title]");
-const linkInput = tripForm.querySelector("[data-link]");
-
-tripPopup.addEventListener("click", closeByOverlay);
-
-const openTripPopupButton = document.querySelector("[data-popup-trip]");
-openTripPopupButton.addEventListener("click", () => {
-    openPopup(tripPopup);
-    document.addEventListener("keyup", closeByEscape);
-})
-
 
 const addTripCard = () => {
     const card = createCard({name: titleInput.value, link: linkInput.value});
@@ -84,23 +72,9 @@ function handleTripFormSubmit(evt) {
     addTripCard();
     closePopup(tripPopup);
     tripForm.reset();
+    submitFormButton.classList.add('popup__button_disabled');
+    submitFormButton.disabled = true;
 }
-tripForm.addEventListener('submit', handleTripFormSubmit);
-
-//Попап открытия карточки
-
-const imagePopup = document.querySelector("#image-popup");
-imagePopup.addEventListener("click", closeByOverlay);
-
-const popupImage = imagePopup.querySelector(".popup__image");
-const setValueImagePopup = (name, link) => {
-    popupImage.src = link;
-    popupImage.alt = `Полное изображение ${name}`;
-    const title = imagePopup.querySelector(".popup__text");
-    title.textContent = name;
-}
-
-//Добавление карточек из массива
 
 const toggleLike = ({target}) => {
     target.classList.toggle("card__button_active");
@@ -111,7 +85,13 @@ const removeCard = ({target}) => {
     card.remove();
 }
 
-const cardTemplate = document.querySelector('#card-template').content;
+const setValueImagePopup = (name, link) => {
+    popupImage.src = link;
+    popupImage.alt = `Полное изображение ${name}`;
+    const title = imagePopup.querySelector(".popup__text");
+    title.textContent = name;
+}
+
 const createCard = ({name, link}) => {
     const card = cardTemplate.querySelector('.trips__item').cloneNode(true)
 
@@ -136,6 +116,33 @@ const createCard = ({name, link}) => {
 
     return card;
 }
+
+
+//Обработчики событий
+
+openProfilePopupButton.addEventListener("click", () => {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileText.textContent;
+    openPopup(profilePopup);
+    document.addEventListener("keyup", closeByEscape);
+})
+profileForm.addEventListener('submit', handleProfileFormSubmit);
+profilePopup.addEventListener("click", closeByOverlay);
+
+
+
+tripPopup.addEventListener("click", closeByOverlay);
+openTripPopupButton.addEventListener("click", () => {
+    openPopup(tripPopup);
+    document.addEventListener("keyup", closeByEscape);
+})
+tripForm.addEventListener('submit', handleTripFormSubmit);
+
+
+
+imagePopup.addEventListener("click", closeByOverlay);
+
+
 
 initialCards.forEach((cardInfo) => {
     const card = createCard(cardInfo);
